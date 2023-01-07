97a295947d7644feeee82a93cb4e962610e06807
Douglas Fanucchi <douglasfanucchi@gmail.com>
2023-01-07 13:06:33 -0300

    adds bubble sort

---
+++ bubble.c
@@ -0,0 +1,23 @@
+void	sort(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
